27d34698353b54ee9fc5445abfae880d0b72a222
lhb200620 <51738606+lhb200620@users.noreply.github.com>
2019-07-04 10:49:35 +0800

    Update lhbmorse.ino.ino

--- lhbmorse.ino.ino
+++ lhbmorse.ino.ino
@@ -5,54 +5,50 @@ void setup(){
   Serial.begin(9600);//设置波特率为9600
 }
 
-String str="";
-void change(int n)
-{
-  switch(n)
-  {
-    case 97:str="*-  ";break;//a
-    case 98:str="-***";break;//b
-    case 99:str="-*-*";break;//c
-    case 100:str="-** ";break;//d
-    case 101:str="*   ";break;//e
-    case 102:str="**-*";break;//f
-    case 103:str="--* ";break;//g
-    case 104:str="****";break;//h
-    case 105:str="**  ";break;//i
-    case 106:str="*---";break;//j
-    case 107:str="-*- ";break;//k
-    case 108:str="*-**";break;//l
-    case 109:str="--  ";break;//m
-    case 110:str="-*  ";break;//n
-    case 111:str="--- ";break;//o
-    case 112:str="*--*";break;//p
-    case 113:str="--*-";break;//q
-    case 114:str="*-* ";break;//r
-    case 115:str="*** ";break;//s
-    case 116:str="-   ";break;//t
-    case 117:str="**- ";break;//u
-    case 118:str="***-";break;//v
-    case 119:str="*-- ";break;//w
-    case 120:str="-**-";break;//x
-    case 121:str="-*--";break;//y
-    case 122:str="--**";break;//z
-    case 32:str="*-*-";break;//空格
-    case 10:str="----";break;//回车
-    default:break;
-  }
-}
-
+String arr="";
+int z=0;
 void loop(){
   while(Serial.available()>0){
-  change(Serial.read());
+  z=Serial.read();
+  switch(z)
+  {
+    case 97:arr="*-  ";break;//a
+    case 98:arr="-***";break;//b
+    case 99:arr="-*-*";break;//c
+    case 100:arr="-** ";break;//d
+    case 101:arr="*   ";break;//e
+    case 102:arr="**-*";break;//f
+    case 103:arr="--* ";break;//g
+    case 104:arr="****";break;//h
+    case 105:arr="**  ";break;//i
+    case 106:arr="*---";break;//j
+    case 107:arr="-*- ";break;//k
+    case 108:arr="*-**";break;//l
+    case 109:arr="--  ";break;//m
+    case 110:arr="-*  ";break;//n
+    case 111:arr="--- ";break;//o
+    case 112:arr="*--*";break;//p
+    case 113:arr="--*-";break;//q
+    case 114:arr="*-* ";break;//r
+    case 115:arr="*** ";break;//s
+    case 116:arr="-   ";break;//t
+    case 117:arr="**- ";break;//u
+    case 118:arr="***-";break;//v
+    case 119:arr="*-- ";break;//w
+    case 120:arr="-**-";break;//x
+    case 121:arr="-*--";break;//y
+    case 122:arr="--**";break;//z
+    case 32:arr="*-*-";break;//space
+    case 10:arr="----";break;//enter
+    default:break;}
   int x=0;
   for(x=0;x<4;x++){
-    if(str[x]=='*')
+    if(arr[x]=='*')
     morse.dot();
-    if(str[x]=='-')
+    if(arr[x]=='-')
     morse.dash();      
     delay(10);         
   }
   delay(350);
-  str="";
+  arr="";
 }}
